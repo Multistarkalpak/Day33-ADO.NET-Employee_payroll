d157ebc078001b1c3ea06a0660449d66d781a88e
Multistarkalpak <kalpakc28@gmail.com>
2022-07-23 01:13:28 +0530

    [kalpak] Aggreate functions

--- ADO.Net/ADO.Net/Program.cs
+++ ADO.Net/ADO.Net/Program.cs
@@ -13,11 +13,15 @@ namespace ADO.NetEmployeeProblem
         {
             EmployeeRepository employeeRepo = new EmployeeRepository();
             Employee_Payroll model = new Employee_Payroll();
-            Console.WriteLine("Enter the choice \n 1.AddingEmployee\n" +
-                " 2.UpdateEmployee\n 3.DeletingTheEmployee\n 4.InsertIntoTwoTables 5.InsertionUsingTsql");
+            Console.WriteLine("Enter the choice \n 0.GetAllEmployeeDetails\n 1.AddingEmployee\n" +
+               " 2.UpdateEmployee\n 3.DeletingTheEmployee\n 4.InsertIntoTwoTables\n" +
+               " 5.InsertionUsingTsql\n 6.RetrivetheEmployeeAccordingToDateRange\n 7.FindSumAvgMinMaxCount");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
+                case 0:
+                    employeeRepo.GetAllEmployees();
+                    break;
                 case 1:
                     model.Name = "kalpak";
                     model.Department = "HR";
@@ -72,9 +76,25 @@ namespace ADO.NetEmployeeProblem
                     employeeRepo.GetAllEmployees();
                     break;
 
-               
-            }
+                case 6:
 
+                    employeeRepo.RetrivetheEmployeeAccordingToDateRange(model);
+
+                    break;
+                case 7:
+
+                    employeeRepo.getAggrigateSumSalary(model);
+                    employeeRepo.getAggrigateAVGSalary(model);
+                    employeeRepo.getAggrigateMAXSalary(model);
+                    employeeRepo.getAggrigateMINSalary(model);
+                    employeeRepo.getAggrigateCOUNTSalary(model);
+                    break;
+
+
+
+            }
         }
+
+        
     }
 }
